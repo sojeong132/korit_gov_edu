f7f3cb7ea1f7ce3b290cf1a5d05606cc4e0e7387
sojeong132 <cleo708028@gmail.com>
2025-05-16 17:23:20 +0900

    feat: 20250516 loop

--- src/main/java/loop/Main8.java
+++ src/main/java/loop/Main8.java
@@ -13,11 +13,26 @@ public class Main8 {
         }
         System.out.println(newWords);
 
+        
+        int a = 10;
+        Integer b = 10;
+//        Integer b = (Integer) a;      // boxing
+//        int c = b;                    // unboxing(autoboxing)
+        System.out.println(a + b);
+
 
         // ints 리스트에 Integer 자료형으로 옮기시오.
         List<Double> doubles = List.of(1.12, 2.23, 3.34, 4.45);
         List<Integer> ints = new ArrayList<>();
 
+        for(Double d : doubles) {
+            ints.add(d.intValue());
+//            ints.add((int)(double)d);
+//            double dd = d;
+//            int i = (int)dd;
+//            ints.add(i);
+        }
+        System.out.println(ints);
 
     }
 }
